2eba01461d1abe43202c8ad57785d3e6ff3d4d87
Marvin Sielenkemper <sielenk@gmx.de>
2016-01-09 20:53:14 +0100

    moved the inline constants into named constants, updated the error handling

--- db.cpp
+++ db.cpp
@@ -3,7 +3,16 @@
 #include "db.hpp"
 
 #include <mariadb/mysql.h>
-#include <iostream>
+#include <sstream>
+
+
+namespace
+{
+  const char* const db_host("192.168.2.51");
+  const char* const db_user("furnace");
+  const char* const db_name("furnace");
+  unsigned int db_port(3306);
+}
 
 
 struct Db::Impl : boost::noncopyable {
@@ -17,12 +26,16 @@ struct Db::Impl : boost::noncopyable {
 
 Db::Db(char* passwd) : m_implPtr(new Impl()) {
   auto& mysql(m_implPtr->mysql);
-  auto const p(mysql_real_connect(&mysql, "192.168.2.51", "furnace",
-                                  passwd, "furnace", 3306, nullptr,
-                                  CLIENT_COMPRESS));
 
-  std::cout << p << " " << &mysql << std::endl << mysql_errno(&mysql)
-            << std::endl << mysql_error(&mysql) << std::endl;
+  if (!mysql_real_connect(&mysql, db_host, db_user, passwd, db_name,
+                                  db_port, nullptr, CLIENT_COMPRESS)) {
+    std::ostringstream buffer;
+
+    buffer << "failed to connect to database: '"
+       << mysql_error(&mysql) << '\'';
+
+    throw std::runtime_error(buffer.str());
+  }
 }
 
 Db::~Db() {
